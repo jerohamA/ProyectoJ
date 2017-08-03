d6e821ac1730d1f586fcbda4a8812da72f251b35
Jeroham <jerohamarboleda@uees.edu.ec>
2017-08-03 13:57:53 -0500

    gggg

--- src/proyecto/Rol.java
+++ src/proyecto/Rol.java
@@ -1,35 +1,107 @@
 package proyecto;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 @ManagedBean
+@SessionScoped
 public class Rol {
-	private int id;
-	private String tipo;
-	
-	public Rol() {
-	}
-	
-	public Rol(int id, String tipo) {
-		super();
-		this.id = id;
-		this.tipo = tipo;
-	}
-	public int getId() {
-		return id;
-	}
+    int id;
+    String tipo;
+    private DataSource dataSource;
+    private String jndiName= "java:comp/env/jdbc/competicion";
+        
+    public Rol(String tipo) throws Exception {
+        super();        
+        this.tipo = tipo;            
+        dataSource = getDataSource();
+    }
+    
+    
+    public Rol() throws Exception {        
+        dataSource = getDataSource();
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }    
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    private DataSource getDataSource() throws NamingException {
+        Context context = new InitialContext();
+        
+        DataSource theDataSource = (DataSource) context.lookup(jndiName);
+        
+        return theDataSource;
+    }
+    
+    public void addRol(Rol rol) throws Exception {
 
-	public String getTipo() {
-		return tipo;
-	}
+        Connection myConn = null;
+        PreparedStatement myStmt = null;
 
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-	
-	
+        try {
+            myConn = getConnection();
+
+            String sql = "insert into roles (tipo) values (?)";
+
+            myStmt = myConn.prepareStatement(sql);
+
+            // set params
+            myStmt.setString(1, rol.getTipo());
+            myStmt.execute();            
+        }
+        finally {
+            close (myConn, myStmt);
+        }
+        
+    }
+    
+    private Connection getConnection() throws Exception {
+
+        Connection theConn = dataSource.getConnection();
+        
+        return theConn;
+    }
+    
+    private void close(Connection theConn, Statement theStmt) {
+        close(theConn, theStmt, null);
+    }
+    
+    private void close(Connection theConn, Statement theStmt, ResultSet theRs) {
+
+        try {
+            if (theRs != null) {
+                theRs.close();
+            }
+
+            if (theStmt != null) {
+                theStmt.close();
+            }
+
+            if (theConn != null) {
+                theConn.close();
+            }
+            
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
 }
--- src/proyecto/RolController.java
+++ src/proyecto/RolController.java
@@ -1,14 +1,50 @@
 package proyecto;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 @SessionScoped
 public class RolController {
-	private Rol rol;
-	private Logger logger = Logger.getLogger(getClass().getName());
-	
+    private Rol roldb;
+    private Logger logger = Logger.getLogger(getClass().getName());
+    
+    
+    public RolController() throws Exception {
+        this.roldb=new Rol();
+    }
+
+    public String addRol(Rol rol) {
+
+        logger.info("Agregando Rol: " + rol);
+
+        try {
+            
+            // add user to the database
+            //agregar aqui comprobacion para rol repetido
+            roldb.addRol(rol);
+            
+        } catch (Exception exc) {
+            // send this to server logs
+            logger.log(Level.SEVERE, "Error agregando rol", exc);
+            
+            // add error message for JSF page
+            addErrorMessage(exc);
+
+            return null;
+        }
+        
+        return "administrador?faces-redirect=true";
+    }
+    
+    private void addErrorMessage(Exception exc) {
+        FacesMessage message = new FacesMessage("Error: " + exc.getMessage());
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+    
 }
